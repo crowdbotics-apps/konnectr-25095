5dc462a9cbeda0e2d2287dc2a6eb1c574b7b7b32
Crowdbotics <team@crowdbotics.com>
2021-03-19 12:51:38 +0000

    Update admin title

--- testkonnectr_25095/urls.py
+++ testkonnectr_25095/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "TESTKONNECTR"
-admin.site.site_title = "TESTKONNECTR Admin Portal"
-admin.site.index_title = "TESTKONNECTR Admin"
+admin.site.site_header = "KONNECTR"
+admin.site.site_title = "KONNECTR Admin Portal"
+admin.site.index_title = "KONNECTR Admin"
 
 # swagger
 api_info = openapi.Info(
